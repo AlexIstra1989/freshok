6989f804e1f2918c658c1b7d447d89b0211fd40c
AlexIstra1989 <alex888fcsm@gmail.com>
2022-03-30 23:59:21 +0300

    added brands block

--- app/js/main.js
+++ app/js/main.js
@@ -24,12 +24,33 @@ $(function() {
  });
 
  $('.brands__slider').slick ({
+  // arrows: false,
+  // dots: false,
   // infinite: true,
-  // fade: true,
   // autoplay: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  //variableWidth: true
+  // slidesToShow: 3,
+  // slidesToScroll: 2,
+  // speed: 2000,
+  // variableWidth: true,
+  responsive:[
+   {
+    breakpoint: 1600,
+     settings:  'unslick'
+    },
+    {
+     breakpoint: 576 ,
+      settings: {
+       arrows: false,
+       dots: false,
+       infinite: true,
+       autoplay: true,
+       slidesToShow: 3,
+       slidesToScroll: 2,
+       speed: 2000,
+       variableWidth: true,
+      }
+     }
+   ]
  });
 
  $('.header-top__burger-btn').on('click', function(){
